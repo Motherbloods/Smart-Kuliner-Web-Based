568319973b6ff873ef3c8a0fa90f8e444a291fdd
Motherbloods <habibskh06@gmail.com>
2025-07-10 18:23:56 +0700

    feat: add logout functionality to sidebar component

--- smartweb/src/pages/PageContent.jsx
+++ smartweb/src/pages/PageContent.jsx
@@ -1,9 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import KontenPage from "./Konten";
 import ProductsList from "./ProductList";
 import Profile from "./Profile";
 import RecipePage from "./Resep";
+import { useEffect } from "react";
+import authService from "../services/AuthServices";
 
 export const PageContent = ({ activeMenu, isSidebarOpen }) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (activeMenu === "logout") {
+            const confirmLogout = window.confirm("Apakah kamu yakin ingin logout?");
+            if (confirmLogout) {
+                authService.logout().then(() => {
+                    console.log('ini login')
+                    navigate("/login");
+                });
+            } else {
+                // Jika batal logout, arahkan kembali ke halaman dashboard atau yang lain
+                navigate("/dashboard");
+            }
+        }
+    }, [activeMenu]);
     const renderContent = () => {
         switch (activeMenu) {
             case 'products':
@@ -19,8 +38,6 @@ export const PageContent = ({ activeMenu, isSidebarOpen }) => {
                 )
             case 'profile':
                 return (<Profile />)
-            case 'logout':
-                return (<div>logout</div>)
             default:
                 return (
                     <div className="p-6">
@@ -33,7 +50,6 @@ export const PageContent = ({ activeMenu, isSidebarOpen }) => {
         }
     };
 
-    console.log(isSidebarOpen)
     return (
         <div
             className={`mt-16 p-6 bg-gray-50 min-h-screen transition-all duration-300 ${isSidebarOpen ? "ml-60" : "ml-16"
